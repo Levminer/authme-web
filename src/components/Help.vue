87dec52c49a8cfc5b39b3173a2352e9f015ae655
Levminer <33373714+Levminer@users.noreply.github.com>
2021-04-17 17:28:42 +0200

    Small fixes

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -19,17 +19,21 @@
 			</ion-card-header>
 			<ion-card-content>
 				<details open class="details1">
-					<summary class="summary1">I'm new to Authme</summary>
+					<summary class="summary1">From Authme </summary>
 
 					<p>Download Authme on your desktop and follow the instructions.</p>
+					<br />
+					<p>
+						After you done go to: Top menu > Advanced > Export. Click Export and than click Save file. After that just import the file you
+						saved.
+					</p>
 				</details>
 
 				<details class="details1">
-					<summary class="summary1">I'm already using Authme</summary>
-
-					<p>
-						Go to: Top menu > Advanced > Export. Click Export and than click Save file. After that just import the file you saved.
-					</p>
+					<summary class="summary1">In Authme Web</summary>
+					<p>Go to the Advaned tab: Upload your QR code(s), then a download window will appear.</p>
+					<br />
+					<p>After that go back to the Codes tab: Upload the file you downloaded.</p>
 				</details>
 			</ion-card-content>
 		</ion-card>
